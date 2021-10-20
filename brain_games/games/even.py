f13ab6a86a3a53d83db04d5ca1421cc9a54a37e4
justpwned <justpwnedfrom@gmail.com>
2021-10-20 19:48:51 +0500

    brain-even game cleanup

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,24 +1,19 @@
 from brain_games.games.utils import ask_question, play_round, welcome_user
-from random import randint
+import random
 
 
-def get_correct_answer(answer):
-    correct_answer = 'yes' if answer % 2 == 0 else 'no'
+def get_correct_answer(number):
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
     return correct_answer
-
-
-def is_game_over(correct_guesss):
-    return correct_guesss == 3
 
 
 def start():
     name = welcome_user(
         'Answer "yes" if the number is even, otherwise answer "no".')
-
     correct_guesses = 0
     running = True
     while running:
-        guessed_number = randint(0, 100)
+        guessed_number = random.randint(0, 100)
         answer = ask_question(guessed_number)
         correct_answer = get_correct_answer(guessed_number)
         running, correct_guesses = play_round(
