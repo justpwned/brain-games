96606ca0465f86c1cba04339986569e13ee265df
justpwned <justpwnedfrom@gmail.com>
2021-10-20 19:49:17 +0500

    brain-calc game implementation

---
+++ brain_games/games/calc.py
@@ -0,0 +1,32 @@
+from brain_games.games.utils import ask_question, play_round, welcome_user
+import operator
+import random
+
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul
+}
+OP_LIST = list(OPERATORS)
+
+
+def get_correct_answer(op1, op, op2):
+    result = OPERATORS[op](op1, op2)
+    return str(result)
+
+
+def start():
+    name = welcome_user('What is the result of the expression?')
+    correct_guesses = 0
+    running = True
+
+    while running:
+        first_operand = random.randint(0, 100)
+        second_operand = random.randint(0, 100)
+        op = random.choice(OP_LIST)
+        expr = f'{first_operand} {op} {second_operand}'
+
+        answer = ask_question(expr)
+        correct_answer = get_correct_answer(first_operand, op, second_operand)
+        running, correct_guesses = play_round(
+            name, answer, correct_answer, correct_guesses)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import brain_games.games.calc
+
+
+def main():
+    brain_games.games.calc.start()
+
+
+if __name__ == '__main__':
+    main()
